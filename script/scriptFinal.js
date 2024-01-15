809549400804343c689aad19fdafeb7d13998190
eviistdl <stadelevelin@gmail.com>
2024-01-15 20:40:48 +0100

    FInished

--- script/scriptFinal.js
+++ script/scriptFinal.js
@@ -51,15 +51,15 @@ function idset(id, string) {
         idset("sekunden", secs);
       }
     }
-  })();
+})();
   
-  // Starte die Stopuhr beim Laden der Seite
-  document.addEventListener("DOMContentLoaded", function() {
-    stoppuhr.start();
-  });
+// Starte die Stopuhr beim Laden der Seite
+document.addEventListener("DOMContentLoaded", function() {
+  stoppuhr.start();
+});
 
-   // Aktualisiere die Stopuhr alle 1000 Millisekunden (1 Sekunde)
-   setInterval(stoppuhr.timer, 1000);
+// Aktualisiere die Stopuhr alle 1000 Millisekunden (1 Sekunde)
+setInterval(stoppuhr.timer, 1000);
   
   
 // Wenn handle 13 berührt, führe checkRezept aus
@@ -95,115 +95,110 @@ function zeigeAlertRight() {
   }
 }
 
+// Image Switcher um zwischen Bildern zu wählen
+  function ImageSwitcher(choices) {
+    var i = 0; // i = nummer des momentanen Bildes im string
+
+    this.Next = function () {
+      hide_current_image();
+      show_next_image();
+    };
+      
+    var hide_current_image = function () {
+      choices[i].style.visibility = "hidden";
+      i += 1;
+      if (i === choices.length) {
+        i = 0;
+      }
+    }
+    var show_next_image = function () {
+      choices[i].style.visibility = "visible";
+    }   
+}
+
+
+//Button Funktion definiert
+var ingredient1 = document.querySelectorAll(".ingredient1");
+var ing1_chooser = new ImageSwitcher(ingredient1);
+  
+var ingredient2 = document.querySelectorAll(".ingredient2");
+var ing2_chooser = new ImageSwitcher(ingredient2);
+  
+var Seal = document.querySelectorAll(".Seal");
+var Seal_chooser = new ImageSwitcher(Seal);
+  
+document.getElementById("ingredient1_button").onclick = function () {
+  ing1_chooser.Next();
+};
+document.getElementById("ingredient2_button").onclick = function () {
+  ing2_chooser.Next();
+};
+document.getElementById("Seal_button").onclick = function () {
+  Seal_chooser.Next();
+};
 //Rezepte Texte
 var RezeptArray = [
     '"Weiß bin ich und knochenhart, mische mich mit Rabenhaaren. Nun gibt es nicht mehr viel zu tun, mach mich zu mit Kerzenblut"',
     '"Schimmer, Glimmer und Veilchen Blätter. zuletzt ein Tuch von einem Ritter. "',
     '"Spitz und scharf bei Bedarf, Löckchen eines langen Barts. Es ist getan so soll es sein, bring den Stopfen von rotem Wein"',
     "",
-  ];
+];
   
-  // Index = Rezept Nummer
-  let Index = -1;
-  // Holen des RezeptDiv-Elements
-  var rezeptDiv = document.getElementById("RezeptDiv");
+// Index = Rezept Nummer
+let Index = -1;
+// Holen des RezeptDiv-Elements
+var rezeptDiv = document.getElementById("RezeptDiv");
   
-  function zeigeRezeptAn() {
-    if (rezeptDiv) {
-      rezeptDiv.innerHTML = "<p>" + RezeptArray[Index] + "</p>";
-    }
-    //Springen zum nächsten Array
-    Index++;
+function zeigeRezeptAn() {
+  if (rezeptDiv) {
+    rezeptDiv.innerHTML = "<p>" + RezeptArray[Index] + "</p>";
   }
+  //Springen zum nächsten Array
+  Index++;
+}
   
-  // Aufruf von Rezepten
-  zeigeRezeptAn();
-  zeigeRezeptAn();
+// Aufruf von Rezepten
+zeigeRezeptAn();
+zeigeRezeptAn();
 
-  // Image Switcher um zwischen Bildern zu wählen
-    function ImageSwitcher(choices) {
-        var i = 0; // i = nummer des momentanen Bildes im string
-
-       this.Next = function () {
-          hide_current_image();
-          show_next_image();
-        };
-      
-        var hide_current_image = function () {
-          choices[i].style.visibility = "hidden";
-          i += 1;
-          if (i === choices.length) {
-            i = 0;
-          }
-        }
-        var show_next_image = function () {
-            choices[i].style.visibility = "visible";
-          }   
-    }
-
-
-  //Button Funktion definiert
-  var ingredient1 = document.querySelectorAll(".ingredient1");
-  var ing1_chooser = new ImageSwitcher(ingredient1);
+//Variablen für alle Bilder Rezept 1
+var zahn = document.getElementById("Zahn");
+var feder = document.getElementById("Feder");
+var wachs = document.getElementById("Wachs");
+//Variablen für alle Bilder Rezept 2
+var kristal = document.getElementById("Kristal");
+var bluete = document.getElementById("Bluete");
+var tuch = document.getElementById("Tuch");
+//Variablen für alle Bilder Rezept 3
+var kralle = document.getElementById("Kralle");
+var haar = document.getElementById("Haar");
+var korken = document.getElementById("Korken");
   
-  var ingredient2 = document.querySelectorAll(".ingredient2");
-  var ing2_chooser = new ImageSwitcher(ingredient2);
+//Welche Bilder gehören zu welchem Rezept
+var bilderRezept0 = [zahn, feder, wachs];
+var bilderRezept1 = [kristal, bluete, tuch];
+var bilderRezept2 = [kralle, haar, korken];
   
-  var Seal = document.querySelectorAll(".Seal");
-  var Seal_chooser = new ImageSwitcher(Seal);
-  
-  document.getElementById("ingredient1_button").onclick = function () {
-    ing1_chooser.Next();
-  };
-  document.getElementById("ingredient2_button").onclick = function () {
-    ing2_chooser.Next();
-  };
-  document.getElementById("Seal_button").onclick = function () {
-    Seal_chooser.Next();
-  };
-  
+bilderRezept0.forEach((bild) => (bild.style.visibility = "hidden"));
+bilderRezept1.forEach((bild) => (bild.style.visibility = "hidden"));
+bilderRezept2.forEach((bild) => (bild.style.visibility = "hidden"));
 
 
-  //Variablen für alle Bilder Rezept 1
-  var zahn = document.getElementById("Zahn");
-  var feder = document.getElementById("Feder");
-  var wachs = document.getElementById("Wachs");
-  //Variablen für alle Bilder Rezept 2
-  var kristal = document.getElementById("Kristal");
-  var bluete = document.getElementById("Bluete");
-  var tuch = document.getElementById("Tuch");
-  //Variablen für alle Bilder Rezept 3
-  var kralle = document.getElementById("Kralle");
-  var haar = document.getElementById("Haar");
-  var korken = document.getElementById("Korken");
-  
-  //Welche Bilder gehören zu welchem Rezept
-  var bilderRezept0 = [zahn, feder, wachs];
-  var bilderRezept1 = [kristal, bluete, tuch];
-  var bilderRezept2 = [kralle, haar, korken];
-  
-  bilderRezept0.forEach((bild) => (bild.style.visibility = "hidden"));
-  bilderRezept1.forEach((bild) => (bild.style.visibility = "hidden"));
-  bilderRezept2.forEach((bild) => (bild.style.visibility = "hidden"));
+function checkRezept() {
+  console.log("checking Rezept ...")
 
-
-
-  function checkRezept() {
-    console.log("checking Rezept ...")
-
-    //Wenn das div das 0.. Array enthält, wird geprüft, ob die richtigen Bilder gezeigt werden.
-    
-    if (rezeptDiv.innerHTML === "<p>" + RezeptArray[0] + "</p>") {
-      // Überprüfen, ob die 3 richtigen Bilder sichtbar sind
-      var sichtbarBildRez0 = bilderRezept0.every((bild) => bild.style.visibility === "visible")
-      if (sichtbarBildRez0) {//Wenn ja, wird die nächste aufgabe angezeigt
-        zeigeAlertRight();
-        zeigeRezeptAn();
+//Wenn das div das 0.. Array enthält, wird geprüft, ob die richtigen Bilder gezeigt werden.
+  if (rezeptDiv.innerHTML === "<p>" + RezeptArray[0] + "</p>") {
+    // Überprüfen, ob die 3 richtigen Bilder sichtbar sind
+    var sichtbarBildRez0 = bilderRezept0.every((bild) => bild.style.visibility === "visible")
+    if (sichtbarBildRez0) {//Wenn ja, wird die nächste aufgabe angezeigt
+      zeigeAlertRight();
+      zeigeRezeptAn();
     } else {
       zeigeAlertWrong();
       }
     }
-  // Prüfen Rezept [1]
+// Prüfen Rezept [1]
     if (rezeptDiv.innerHTML === "<p>" + RezeptArray[1] + "</p>") {
       // Überprüfen, ob die 3 richtigen Bilder sichtbar sind
       var sichtbarBildRez1 = bilderRezept1.every((bild) => bild.style.visibility === "visible")
@@ -216,7 +211,7 @@ var RezeptArray = [
         }
     }
 
-  // Prüfen Rezept [2]
+// Prüfen Rezept [2]
   if (rezeptDiv.innerHTML === "<p>" + RezeptArray[2] + "</p>") {
     // Überprüfen, ob die 3 richtigen Bilder sichtbar sind
     var sichtbarBildRez2 = bilderRezept2.every((bild) => bild.style.visibility === "visible")
@@ -247,7 +242,6 @@ var RezeptArray = [
       }
     }
 }
-
 
 
 //Sobald alle Aufgaben bestanden:
@@ -292,5 +286,3 @@ var RezeptArray = [
     // Setze den Interval für die Anzeige zufälliger Bilder
     setInterval(showRandomImage, interval);
   }
-
-
